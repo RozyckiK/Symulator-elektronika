269c03bf226ba2dcb16645c0a7807a60a0471a17
RozyckiK <krozycki7@gmail.com>
2022-11-06 22:34:04 +0100

    1

--- symele/s184953/Game.java
+++ symele/s184953/Game.java
@@ -1,7 +1,26 @@
 package symele.s184953;
 
-public class Gra {
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 800, HEIGHT = WIDTH / 12 * 9;
+
+    public Game(){
+        new Window(WIDTH, HEIGHT, "Symulator elektronika", this);
+    }
+
+    public synchronized void start(){
+
+    }
+
+    public void run(){
+        
+    }
+
     public static void main(String args[]){
 
+        new Game();
     }
 }
--- symele/s184953/Window.java
+++ symele/s184953/Window.java
@@ -1,7 +1,23 @@
 package symele.s184953;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
-public class Okno extends Canvas {
-    private static final long serialVersionUID = -240840600533728354L;
+public class Window extends Canvas {
+
+    public Window(int width, int height, String title, Game game){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+    }
 }
